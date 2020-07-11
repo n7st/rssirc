0609e0091eefee92c78b7ec9937be9318dc8e401
Mike Jones <mike@netsplit.org.uk>
2020-07-11 16:09:58 +0100

    Add missing "cmd" directory

---
+++ cmd/rssirc/main.go
@@ -0,0 +1,30 @@
+// Package main contains the application's entrypoint.
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+
+	"github.com/n7st/rssirc/internal/app/rssbot"
+	"github.com/n7st/rssirc/internal/app/rssfeed"
+	"github.com/n7st/rssirc/internal/pkg/util"
+)
+
+// main sets up an IRC bot and many RSS feed pollers.
+func main() {
+	config := util.NewConfig()
+	logger := logrus.New()
+
+	logger.SetLevel(config.LogLevel)
+	logger.WithFields(logrus.Fields{
+		"level": config.LogLevel.String(),
+	}).Info("Set log level")
+
+	bot := rssbot.Init(config, logger)
+
+	for _, rss := range config.RSS {
+		poller := rssfeed.NewPoller(rss, bot, logger)
+		poller.Poll()
+	}
+
+	bot.Connection.Loop()
+}
